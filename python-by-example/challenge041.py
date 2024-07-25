534413f9ee13c572de2db7ba92e2bf349209eded
mandi628 <amanda.aumann@gmail.com>
2024-07-24 23:10:25 -0400

    New challenge

--- python-by-example/challenge041.py
+++ python-by-example/challenge041.py
@@ -6,6 +6,15 @@
 # Chapter 5: For Loop
 
 print("Challenge 041")
-# Ask the user to enter their name and a number. If the numb er is less than 10, then display
+# Ask the user to enter their name and a number. If the number is less than 10, then display
 # their name that number of times; otherwise display the message "Too high" three times.
 
+name = input("Enter your name: ")
+num = int(input("Enter a number: "))
+
+if num < 10:
+    for i in range(0, num):
+        print(name)
+else:
+    for i in range(0, 3):
+        print("Too high!")
