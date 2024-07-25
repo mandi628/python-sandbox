500d2fd56bfb5e482da3c2a57a4dfe86801feb02
mandi628 <amanda.aumann@gmail.com>
2024-07-24 23:11:17 -0400

    Organize copy scripts

---
+++ python-by-example/copy2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Enter the file you want to copy: "
+read oldFile
+
+chmod -x $oldFile
+
+echo "Enter the new file name (with no filetype): "
+read newFile
+
+cp $oldFile ../../my_sandbox/bash/bash-by-example/$newFile.sh
+cp $oldFile ../../my_sandbox/ruby/ruby-by-example/$newFile.rb
+cp $oldFile ../../my_sandbox/js/js-by-example/$newFile.js
+
+chmod +x $oldFile
+
+# This new version requires less typing. It still copies the file to the required folders,
+# but it automatically adds the filetype. Not sure how to automatically remove the ".py"
+# from the original filename yet.
