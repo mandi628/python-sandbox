ac21a5aa86bd91da07e613303151c4a728ff429b
mandi628 <amanda.aumann@gmail.com>
2024-07-24 23:09:27 -0400

    New challenge

--- python-by-example/challenge036.py
+++ python-by-example/challenge036.py
@@ -9,3 +9,8 @@ print("Challenge 036")
 # Alter program 035 so that it will ask the user to enter their name and a number and then 
 # display their name that number of times.
 
+name = input("Enter your name: ")
+num = int(input("Enter a number: "))
+
+for i in range(1, num+1):
+    print("%s, %d" % (name, i))
