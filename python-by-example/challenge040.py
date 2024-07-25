5b38aebe492f0765127508f56481a3190bc127d2
mandi628 <amanda.aumann@gmail.com>
2024-07-24 23:10:19 -0400

    New challenge

--- python-by-example/challenge040.py
+++ python-by-example/challenge040.py
@@ -9,3 +9,7 @@ print("Challenge 040")
 # Ask for a number below 50 and then count down from 50 to that number, making sure you
 # show the number they entered in the output.
 
+num = int(input("Enter a number less than 50: "))
+
+for x in range(50, num-1, -1):
+    print(x)
