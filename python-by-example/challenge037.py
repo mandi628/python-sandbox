db61f130a4eac381ff9fa71e04f3c04a9b3ff20e
mandi628 <amanda.aumann@gmail.com>
2024-07-24 23:09:41 -0400

    New challenge

--- python-by-example/challenge037.py
+++ python-by-example/challenge037.py
@@ -7,3 +7,8 @@
 
 print("Challenge 037")
 # Ask the user to enter their name and display each letter in their name on a separate line.
+
+name = input("Enter your name: ")
+
+for i in name:
+    print(i)
