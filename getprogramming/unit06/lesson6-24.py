9df6df235348d59a20218d46fa6cdae1e0d4c91c
mandi628 <mandi628.codes@gmail.com>
2024-07-30 13:53:13 -0400

    Complete Lesson 24 - notes only

---
+++ getprogramming/unit06/lesson6-24.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+# Get Programming: Learn to Code with Python, by Ana Bell
+# borrowed from library 2024-07-05; renewed 2024-07-27
+
+# Unit 6: Working with mutable data types
+# Lesson 24: Mutable and Immutable Objects
+
+Mutable objects can change their value (lists, dictionaries, etc.)
+
+Immutable objects can't change their value (strings, integers, floats, Booleans, tuples).
